2358b5ecb571cd071d944e36f0b20465172e3d81
James <jharr651@students.kennesaw.edu>
2022-03-22 11:15:06 -0400

    cometchat ui kit upgrade

--- AdventureTogether/src/CometChatWorkspace/src/components/Calls/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CometChatIncomingCall } from "./CometChatIncomingCall";
-export { default as CometChatOutgoingCall } from "./CometChatOutgoingCall";
-export { default as CometChatIncomingDirectCall } from "./CometChatIncomingDirectCall";
-export { default as CometChatOutgoingDirectCall } from "./CometChatOutgoingDirectCall";
